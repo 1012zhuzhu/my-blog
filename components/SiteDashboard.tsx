b1b6b0676827cb40c552656d06395f16bfee3926
itheima <itheima@itcast.cn>
2026-08-12 13:56:24 +0800

    完成介绍框架组件

---
+++ components/SiteDashboard.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { siteConfig } from "@/siteConfig"
+import { log } from "node:console";
+
+export default function SitDashboard() {
+    const [timeStr,setTimeStr] = useState("");
+    const [updatetime,setUpdatetime] = useState("");
+
+    const START_TIME = new Date(siteConfig.buildDate || "2026-07-29T00:00:00");
+
+    
+    useEffect(() =>{
+        const updateTime = () =>{
+            const now = new Date()
+            setTimeStr(now.toLocaleTimeString('en-us',{hour12: false,hour : '2-digit',minute : '2-digit', second : '2-digit'}))
+            const diff = now.getTime() - START_TIME.getTime();
+            const days = Math.floor(diff/(1000 * 60 * 60 * 24));
+            const hours = Math.floor(diff/(1000*60*60))
+
+            return setUpdatetime(`${days}天${hours}小时`)
+        };
+        updateTime();
+        const timer = setInterval(updateTime,1000)
+        return ()=> clearInterval(timer)
+    },[START_TIME])
+
+    return(
+        <div className="md:col-span-12 rounded-3xl bg-white/40 dark:bg-slate-800/50 backdrop-blur-md border border-white/40 dark:border-white/10 shadow-xl overflow-hidden flex flex-col md:flex-row items-stretch transition-colors duration-700 h-auto md:h-20 group">
+            <div className="bg-slate-900 dark:bg-black text-white px-8 py-4 md:py-0 flex items-center justify-center font-mono text-2xl md:text-3xl font-black tracking-widest shadow-inner relative overflow-hidden group-hover:text-indigo-400 transition-colors">
+                <div className="absolute inset-0 bg-gradient-to-b from-white/10 to-transparent pointer-events-none:"></div>
+                {timeStr || '00:00:00'}
+                <div className="absolute left-0 right-0 top-1/2 h-px bg-black-50"></div>
+            </div>
+
+            <div className="flex-1 px-6 py-4 md:py-0 flex flex-wrap iitems-center justify-between gap-4 text-xs md:text-sm font-bold text-slate-600 dark:text-slate-300">
+                <div className="flex items-center gap-2">
+                    <span className="w-2 h-2 rounded-full bg-green-500 animate-pulse"></span>
+                    <span>已经开始：
+                        <span className="text-indigo-600 dark:text-indigo-400 font-black">{updatetime}</span>
+                    </span>
+
+
+                     <div className="flex gap-2">
+                        {siteConfig.footerBadges?.map((badge, index) => (
+                        <span
+                        key={index}
+                        className="px-2 py-1 bg-white/50 dark:bg-slate-700/50 rounded-md shadow-sm flex items-center gap-1 border border-white/40 dark:border-slate-600"
+                        >
+                        <svg className={`w-3.5 h-3.5 ${badge.color}`} fill="currentColor" viewBox="0 0 24 24" dangerouslySetInnerHTML={{ __html: badge.svg }} />
+                        {badge.name}
+                        </span>
+                    ))}
+                    </div>
+
+                    {/* 备案信息 (🌟 从 siteConfig 读取链接和名称) */}
+                    {/* {siteConfig.icpConfig && (
+                    <a
+                        href={siteConfig.icpConfig.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="hover:text-indigo-500 transition-colors border-b border-dashed border-slate-400 dark:border-slate-500 pb-0.5"
+                    >
+                        {siteConfig.icpConfig.name}
+                    </a>
+                    )} */}
+                </div>
+            </div>
+        </div>
+    );
+}
